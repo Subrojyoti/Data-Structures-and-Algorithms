853194f863dbc11b46bfc68036bdcb64a740a4cd
Subrojyoti Paul <subrojyotipaul@gmail.com>
2024-03-31 22:24:51 +0530

    update functions addNodeAtTail and deleteNodeFromTail

--- LinkedList/Singly_Linked_List/SLL_creation.cpp
+++ LinkedList/Singly_Linked_List/SLL_creation.cpp
@@ -15,15 +15,15 @@ class Node{
     Node(): data(0), next(NULL){}   // default constructor
     Node(T value): data(value), next(NULL){}  // parameterized constructor
     Node(T value, Node* link): data(value), next(link){}   // parametrized constructor
+    ~Node() {delete next;}
 };
 
 template <class T>
 class SLinkedList{
-    Node<T>* head;
     public:
-    void setHead(Node* address) { head = address; }
-    Node* getHead() { return head; }
+    Node<T>* head;
     SLinkedList() { head = NULL; }
+    ~SLinkedList();
     void addNodeAtHead(T);
     void deleteNodeFromHead();
     void addNodeAtTail(T);
@@ -31,3 +31,69 @@ class SLinkedList{
     void deleteNodeFromMiddle();
     void CountNodes();
 };
+
+template <class T>
+SLinkedList<T> :: ~SLinkedList(){
+    Node<T>* temp = head;
+    while( !temp ){
+        head = head->getNext();
+        delete temp;
+        temp = head;
+    }
+}
+template <class T>
+void SLinkedList<T> :: addNodeAtHead(T data){
+    Node<T>* newNode = new Node<T>(data);
+    if(head == NULL)
+        head = newNode;
+    else{
+        Node<T>* temp = head;
+        while(temp->getNext() != NULL){
+            temp = temp->getNext();
+        }
+        temp->setNext(newNode);
+    }
+}
+
+template <class T>
+void SLinkedList<T> :: deleteNodeFromHead(){
+    if(head == NULL){
+        cout << "The list is already empty."<< endl;
+        return;
+    }
+    else{
+        Node<T>* temp = head;
+        head = head->getNext();
+        delete temp;
+    }
+}
+
+template<class T>
+void SLinkedList<T> :: addNodeAtTail(T data){
+    Node<T>* newNode = new Node<T>(data);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    else{
+        Node<T>* temp = head;
+        while(temp->getNext()){
+            temp = temp->getNext();
+        }
+        temp->setNext(newNode);
+    }
+}
+template <class T>
+void SLinkedList<T> :: deleteNodeFromTail(){
+    if(head == NULL){
+        cout << "The list is already empty." << endl;
+        return;
+    }
+    else{
+        Node<T>* temp = head;
+        while(temp->getNext()){
+            temp = temp->getNext();
+        }
+        delete temp;
+    }
+}
