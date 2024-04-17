151f94ad349d8e9b6baac8653fceec2ad3f3ac23
Subrojyoti Paul <subrojyotipaul@gmail.com>
2024-04-17 23:52:19 +0530

    dynammic array construction

---
+++ Dynammic Array/array_skeleton.h
@@ -0,0 +1,23 @@
+#ifndef ARRAYSKELETON_H
+#define ARRAYSKELETON_H
+
+template <class T>
+class TemplateArray{
+    T *arr;
+    long capacity;
+    long numOfElements;
+    void resize();
+
+    public:
+    static const int DefaultArraySize = 1;
+
+    // constructors
+    TemplateArray(int capacity = DefaultArraySize); // default constructor
+    TemplateArray(T *thisArray, long thisArraySize); // parameterised constructor
+    TemplateArray(const TemplateArray <T>&); // copy constructor
+
+    // destructor
+    ~TemplateArray();
+};
+
+#endif
